1c093a56760d7e54140faa1353373f8a7ffa8f26
Darito <davidalonso991@gmail.com>
2022-02-08 15:02:23 -0600

    Creacion de funciones en componente app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,66 @@
 import { useState } from "react";
 import FinanceSection from "./components/FinanceSection";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 import exampleData from "./data";
 function App() {
   const data = exampleData;
   const [financeData, setFinanceData]: null | any = useState(null);
-  function hideHeader() {
-    setFinanceData(data);
+  const [showHeader, setShowHeader] = useState(true);
+  function changeShowHeader() {
+    setShowHeader(!showHeader);
+  }
+  const [showForm, setShowForm] = useState(false);
+  function changeShowForm() {
+    setShowForm(!showForm);
+  }
+  function hideHeaderAndShowForm() {
+    if (showHeader === true) {
+      changeShowHeader();
+    }
+    if (showForm !== true) {
+      changeShowForm();
+    }
+  }
+  const [type, setType] = useState("income");
+  function changeTypeOfForm(type: string) {
+    setType(type);
+  }
+  type typeOfCostOrIncome = {
+    title: string;
+    value: number;
+  };
+  function addCost(newCost: typeOfCostOrIncome) {
+    let dataTemp = financeData;
+    console.log(dataTemp, newCost);
+  }
+  function addIncome(newIncome: typeOfCostOrIncome) {
+    let dataTemp = financeData;
+    console.log(dataTemp, newIncome);
+  }
+  function addNewCostOrIncome(
+    newCostOrIncome: typeOfCostOrIncome,
+    type: string
+  ) {
+    if (type === "income") {
+      addIncome(newCostOrIncome);
+    } else {
+      addCost(newCostOrIncome);
+    }
   }
 
   return (
     <div className="bg-slate-900 h-screen flex justify-center">
-      {financeData ? (
-        <FinanceSection data={data} />
+      {showHeader ? (
+        <Header HideHeader={() => hideHeaderAndShowForm()} />
       ) : (
-        <Header HideHeader={() => hideHeader()} />
+        <FinanceSection data={data} />
+      )}
+      {showForm ? (
+        <Form type={type} action={""} changeShowForm={() => changeShowForm()} />
+      ) : (
+        ""
       )}
     </div>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,54 @@
-function Form(props: { type: string; action: any }) {
-  const { type, action } = props;
+function Form(props: { type: string; action: any; changeShowForm: any }) {
+  const { type, action, changeShowForm } = props;
 
   function submitForm(e: any) {
     action(e);
   }
+  function closeForm(event: any) {
+    event.preventDefault();
+    changeShowForm();
+  }
+  const closeIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 0 24 24"
+      width="24px"
+      fill="#FFFFFF"
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+    </svg>
+  );
 
   return (
     <form
-      className="fixed top-0 bottom-0 left-0 right-0 flex justify-center align-middle bg-slate-700/20 blur-sm"
+      className="fixed top-0 bottom-0 right-0 left-0 flex justify-center items-center bg-slate-700/70"
       onSubmit={(e) => submitForm(e)}
     >
-      <div className="p-4 rounded-lg bg-slate-900">
+      <div className=" flex flex-col justify-center items-stretch gap-2 p-4 rounded-lg bg-slate-900 h-fit text-slate-100">
+        <button className="self-end" onClick={closeForm}>
+          {closeIcon}
+        </button>
         <label htmlFor="title">Nombre</label>
-        <input type="text" name="title" placeholder="Juguete nuevo" />
+        <input
+          className="py-1 px-2 rounded-md text-slate-900"
+          type="text"
+          name="title"
+          placeholder="Juguete nuevo"
+        />
         <label htmlFor="value">Valor</label>
-        <input type="number" name="value" placeholder="200" />
+        <input
+          className="py-1 px-2 rounded-md text-slate-900"
+          type="number"
+          name="value"
+          placeholder="200"
+        />
         <button
           className={
             type === "income"
-              ? "border-2 border-green-900 rounded-sm"
-              : "border-2 border-red-900 rounded-sm"
+              ? "p-1 bg-green-900 text-slate-100 rounded-md"
+              : "p-1 bg-red-900 text-slate-100 border-2 border-green-900 rounded-md"
           }
           type="submit"
         >
